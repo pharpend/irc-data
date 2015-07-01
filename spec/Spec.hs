377d28170650de77fe9f3335b0cb2a4a6fea7d0f
Peter Harpending <peter@harpending.org>
2015-07-01 01:03:30 -0600

    Add tests for hostname parsing.

--- spec/Spec.hs
+++ spec/Spec.hs
@@ -1,16 +1,19 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 -- Copyright 2015 Peter Harpending
 -- 
--- Licensed under the Apache License, Version 2.0 (the "License");
--- you may not use this file except in compliance with the License.
--- You may obtain a copy of the License at
+-- Licensed under the Apache License, Version 2.0 (the "License"); you may not
+-- use this file except in compliance with the License.  You may obtain a copy
+-- of the License at
 -- 
 --  http://www.apache.org/licenses/LICENSE-2.0
 -- 
 -- Unless required by applicable law or agreed to in writing, software
--- distributed under the License is distributed on an "AS IS" BASIS,
--- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
--- See the License for the specific language governing permissions and
--- limitations under the License.
+-- distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+-- WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+-- License for the specific language governing permissions and limitations under
+-- the License.
 
 -- | 
 -- Module      : Main
@@ -23,7 +26,92 @@
 
 module Main where
 
+import Data.ByteString.Char8 (ByteString)
+import qualified Data.ByteString.Char8 as B
+import Data.IRC
 import Test.Hspec
-       
+import Test.Hspec.Expectations.Contrib
+import Test.QuickCheck
+
+#if !(MIN_VERSION_base(4,8,0))
+import Control.Applicative
+import Data.Monoid
+#endif
+
 main :: IO ()
-main = hspec $ do return ()
+main =
+  hspec $
+  parallel $
+  do context "Hostnames" $
+       do context "Supposedly valid hostnames" $
+            mapM_ testValidHnom
+                  ["irc.freenode.net"
+                  ,"chat.freenode.net"
+                  ,"foo.bar.baz"
+                  ,"irc.oftc.net"
+                  ,"blah.bleh"
+                  ,"foo.bar.baz"
+                  ,"foobar.baz.qu123ux"
+                  ,"foo-bar.baz.qu123ux"
+                  ,"foobar.baz.qu-123ux"
+                  ,"foo-bar.baz.qu-123ux"
+                  ,"fero.tui.sceleris"]
+          context "Supposedly invalid hostnames" $
+            do mapM_ testInvalidHnom
+                     ["irc.freenode.net."
+                     ,"chat-.freenode.net"
+                     ,"foo,.--8bar.baz"
+                     ,"irc.oftc-.net"
+                     ,"blah.blehj61.--"
+                     ,"foo.bar.baz--8-"
+                     ,"foobar.baz..qu123ux"
+                     ,"foo-.-bar.baz.qu123ux"
+                     ,"foob.-.ar.baz.qu-123ux"
+                     ,"foo-.bar.baz.qu-123ux"
+                     ,"fer.-o.tui.sceleris"]
+               specify "Randomly generated invalid hostnames should be invalid" $
+                 property $
+                 \(InvalidHostName x) ->
+                   shouldNotBe (fmap unHostName (parseHostName x))
+                               (Right x)
+
+testValidHnom :: ByteString -> Spec
+testValidHnom s =
+  specify (mappend (B.unpack s) " should be a valid hostname") $
+  do shouldBe (fmap unHostName (parseHostName s))
+              (Right s)
+
+testInvalidHnom :: ByteString -> Spec
+testInvalidHnom s =
+  specify (mappend (B.unpack s) " should not be a valid hostname") $
+  shouldNotBe (fmap unHostName (parseHostName s))
+              (Right s)
+
+newtype InvalidHostName = InvalidHostName ByteString
+  deriving (Show, Eq)
+
+newtype HostNamePart = HostNamePart { unHp :: ByteString}
+  deriving (Show, Eq)
+
+instance Arbitrary InvalidHostName where
+  arbitrary =
+    do hnparts <-
+         suchThat arbitrary (\x -> length x > 0)
+       pure (InvalidHostName
+               (B.intercalate "."
+                              (fmap unHp hnparts)))
+
+instance Arbitrary HostNamePart where
+  arbitrary =
+    fmap HostNamePart $
+    suchThat arbitrary $
+    \bytes ->
+      or [B.null bytes
+         ,B.head bytes == '-'
+         ,B.last bytes == '-'
+         ,B.any (not .
+                 (`elem` (mconcat [['A' .. 'Z'],['a' .. 'z'],['0' .. '9'],"-"])))
+                bytes]
+
+instance Arbitrary ByteString where
+  arbitrary = fmap B.pack arbitrary
